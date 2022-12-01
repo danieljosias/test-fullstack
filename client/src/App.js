8604f93b58ccea596cc293d912a0d425cd936db3
Daniel Josias <daniieljosiaslima@gmail.com>
2022-12-01 08:46:59 -0300

    feat: a cabeçalho da página está feito

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { GlobalStyle} from './styles/global';
 import { Routes } from './routes'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <GlobalStyle/>
       <Routes/>
     </div>
